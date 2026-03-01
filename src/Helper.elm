f3ce39441d96dbd2fa0824e8ac7b3df2290c24b2
Joel-Acvedo~ <up230571@alumnos.upa.edu.mx>
2026-03-01 11:45:14 -0600

    A ver si quiere

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,62 @@
 module Helper exposing (..)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+list : List Float
+list =
+    [ 3.1, -9, 7 ]
+
+
+estadoCalif : Float -> GradeStatus
+estadoCalif cal =
+    if cal >= 7 then
+        Approved
+
+    else if cal < 0 then
+        Pending
+
+    else
+        Failed
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade lista =
+    List.map estadoCalif lista
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+listaAvion : List AirplaneStatus
+listaAvion =
+    [ OnTime, Boarding, Delayed, Cancelled ]
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        OnTime ->
+            "Esperar"
+
+        Delayed ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+        Cancelled ->
+            "Pedir reembolso"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction lista =
+    List.map airplaneScheduleAction lista
